4e8d5ec5c9becfb179d2032c36d3f5c22dc1080b
aksakalhurrem <irmakpdr@gmail.com>
2024-12-21 12:30:47 -0800

    US_10/12 Last Commit2

--- src/test/java/Runner/Runner.java
+++ src/test/java/Runner/Runner.java
@@ -9,8 +9,8 @@ import org.junit.runner.RunWith;
                 "json:target/cucumber.json",},
         features = "src/test/resources/features",
         glue = "stepdefinitions",
-        tags = "@hurrem",
-        dryRun = false
+        tags = "@Add",
+        dryRun = true
 
 
 )
