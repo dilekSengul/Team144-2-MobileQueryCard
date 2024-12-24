d8f006e5705010eeadc0ee4eb517250ac3a65beb
Cpik <cepik.kbrnr@gmail.com>
2024-12-24 14:29:07 +0300

    stepdefDublicate

--- src/test/java/Runner/Runner.java
+++ src/test/java/Runner/Runner.java
@@ -9,8 +9,8 @@ import org.junit.runner.RunWith;
                 "json:target/cucumber.json",},
         features = "src/test/resources/features",
         glue = "stepdefinitions",
-        tags = "@us23",//@Add
-        dryRun = false//true
+        tags = "@Add",
+        dryRun = true
 
 
 )
